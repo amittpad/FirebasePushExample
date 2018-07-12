383d413a1bfa70ad37d12717b6e6704797a1839a
amit_pramanik <amit.mca93@gmail.com>
2018-07-13 05:06:57 +0530

    Generating Access Token and Receiving Messages

---
+++ app/src/main/java/com/example/amittpad/firebasepushexample/Constants.java
@@ -0,0 +1,7 @@
+package com.example.amittpad.firebasepushexample;
+
+public class Constants {
+    public static final String CHANNEL_ID = "amit_tpad_101";
+    public static final String CHANNEL_NAME = "AMIT TPAD";
+    public static final String CHANNEL_DESCRIPTION = "Android Developer";
+}
